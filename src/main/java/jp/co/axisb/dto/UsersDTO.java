eda876888d72471f53b3d83c30498fe04b16d0bb
morio-j <morio-j@AXPC-1554N.TTRDOM.local>
2025-06-12 15:20:54 +0900

    できた

--- src/main/java/jp/co/axisb/dto/UsersDTO.java
+++ src/main/java/jp/co/axisb/dto/UsersDTO.java
@@ -1,5 +1,36 @@
 package jp.co.axisb.dto;
 
 public class UsersDTO {
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private int user_id;
+	private int password;
+	private String name;
+	private String address;
+	
+	
 
 }
