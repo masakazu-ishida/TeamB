4a866365b34b8111ae1baece31397a1abbd65b43
terasawa-r <terasawa-r@AXPC-1477N.TTRDOM.local>
2025-06-12 15:09:35 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

---
+++ src/main/java/jp/co/axisb/dto/ItemsInCartDTO.java
@@ -0,0 +1,37 @@
+package jp.co.axisb.dto;
+
+import java.sql.Date;
+
+public class ItemsInCartDTO {
+	private String user_id;
+	private int item_id;
+	private int amount;
+	private Date booked_date;
+	
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getItem_id() {
+		return item_id;
+	}
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public Date getBooked_date() {
+		return booked_date;
+	}
+	public void setBooked_date(Date booked_date) {
+		this.booked_date = booked_date;
+	}
+
+}
