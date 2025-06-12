7434653af4e3a8671d4843eba5934bd856ab9c72
morio-j <morio-j@AXPC-1554N.TTRDOM.local>
2025-06-12 15:22:18 +0900

    できた

---
+++ src/main/java/jp/co/axisb/dto/AdministrarorsDTO.java
@@ -0,0 +1,27 @@
+package jp.co.axisb.dto;
+
+public class AdministrarorsDTO {
+	
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private int admin_id;
+	private int password;
+	private String name;
+
+}
