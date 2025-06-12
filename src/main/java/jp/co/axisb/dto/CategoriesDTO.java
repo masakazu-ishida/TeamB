e5629f78ad2a61a7aee37b9da8472da4f61e378c
okada.y <okada.y@AXPC-1479N.TTRDOM.local>
2025-06-12 15:10:56 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

---
+++ src/main/java/jp/co/axisb/dto/CategoriesDTO.java
@@ -0,0 +1,22 @@
+package jp.co.axisb.dto;
+
+public class CategoriesDTO {
+	private int cotegory_id;
+	private String name;
+
+	public int getCotegory_id() {
+		return cotegory_id;
+	}
+
+	public void setCotegory_id(int cotegory_id) {
+		this.cotegory_id = cotegory_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
