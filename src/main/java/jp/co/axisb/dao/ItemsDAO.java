c8eec92274993dd939a00ed83acaaa9916afa7f6
tanaka-yu <tanaka-yu@AXPC-1476N.TTRDOM.local>
2025-06-13 10:30:16 +0900

    findById

---
+++ src/main/java/jp/co/axisb/dao/ItemsDAO.java
@@ -0,0 +1,39 @@
+package jp.co.axisb.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jp.co.axisb.dto.ItemsDTO;
+
+public class ItemsDAO extends BaseDAO{
+	
+	public ItemsDAO(Connection conn) {
+		super(conn);
+	}
+
+		public ItemsDTO findById(int itemId) throws SQLException {
+
+			String sql = "SLECT * FROM items WHERE itemId = ?";
+
+			ItemsDTO dto = null;
+
+				try (PreparedStatement ps = conn.prepareStatement(sql)) {
+
+					ps.setInt(1, itemId);
+					
+					ResultSet rs = ps.executeQuery();
+
+					if (rs.next()) {
+						dto = new ItemsDTO();
+
+						dto.setItemId(rs.getInt("item_id"));
+					}
+				}
+
+			return dto;
+
+		}
+
+}
