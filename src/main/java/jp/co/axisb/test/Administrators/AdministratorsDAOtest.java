c0ce0abf7473be4b37c90082d269eb10a1ef0d25
terasawa-r <terasawa-r@AXPC-1477N.TTRDOM.local>
2025-06-13 14:26:02 +0900

    わ

--- src/main/java/jp/co/axisb/test/Administrators/AdministratorsDAOtest.java
+++ src/main/java/jp/co/axisb/test/Administrators/AdministratorsDAOtest.java
@@ -20,19 +20,21 @@ class AdministratorsDAOtest {
 		try(Connection conn = ConnectionUtil.getConnectionForJUnit()) {
 			AdministratorsDAO dao = new AdministratorsDAO(conn);
 
+			 //正しくDTOにレコードの値を詰めてるか確認する
 				AdministratorsDTO dto = dao.findById("admin");
 				
 				assertEquals("admin", dto.getAdminId());
 				assertEquals("admin", dto.getPassword());
 				assertEquals("管理者", dto.getName());
-			
+			//存在しない主キーを指定した場合、NULLを返す事を確認する
+				dto = dao.findById("aaaa");
+				assertNull(dto);
+				
 		} catch (Exception e) {
 			// TODO: handle exception
 			fail(e.getMessage());
 		}
 	}
-	
-	
 	
 	
 	@Test
@@ -59,26 +61,6 @@ class AdministratorsDAOtest {
 			// TODO: handle exception
 			e.printStackTrace();
 			
-			fail(e.getMessage());
-		}
-	}
-	
-	void testFindByIdNull() {
-		try(Connection conn = ConnectionUtil.getConnectionForJUnit()) {
-			AdministratorsDAO dao = new AdministratorsDAO(conn);
-			
-			try {
-				AdministratorsDTO dto = dao.findById("afj");
-				assertNull(dto);
-				
-			} catch (Exception e) {
-				// TODO: handle exception
-				fail(e.getMessage());
-			}
-			
-		} catch (SQLException e) {
-			// TODO: handle exception
-			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
---
+++ src/main/java/jp/co/axisb/test/Categories/Categories.java
@@ -0,0 +1,63 @@
+package jp.co.axisb.test.Categories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import jp.co.axisb.dao.CategoriesDAO;
+import jp.co.axisb.dto.CategoriesDTO;
+import jp.co.axisb.util.ConnectionUtil;
+
+class Categories {
+
+	@Test
+	void testfindById() {
+		try(Connection conn = ConnectionUtil.getConnectionForJUnit()) {
+			CategoriesDAO dao = new CategoriesDAO(conn);
+			
+			//正しくDTOにレコードの値を詰めてるか確認する
+			CategoriesDTO dto = dao.findById(1);
+			
+			assertEquals(0, dto.getCotegoryId());
+			assertEquals("すべて", dto.getName());
+			
+			//存在しない主キーを指定した場合、NULLを返す事を確認する
+			dto = dao.findById(6);
+			assertNull(dto);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}	
+	}
+	@Test
+	void  testFindAll() {
+		try(Connection conn = ConnectionUtil.getConnectionForJUnit()) {
+			CategoriesDAO dao = new CategoriesDAO(conn);
+			
+			try {
+				List<CategoriesDTO> list = dao.findAll();
+				assertEquals(3, list.size());
+				CategoriesDTO dto = list.get(3);
+				
+				assertEquals(0, dto.getCotegoryId());
+				assertEquals("すべて", dto.getName());
+				
+				
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				fail(e.getMessage());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+			fail(e.getMessage());
+		}
+	}
+}
+
