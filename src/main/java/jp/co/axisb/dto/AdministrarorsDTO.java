85f562ec3f3f872f31f7a8b8f0b0d4283dc27a07
morio-j <morio-j@AXPC-1554N.TTRDOM.local>
2025-06-13 09:27:06 +0900

    あ

--- src/main/java/jp/co/axisb/dto/AdministrarorsDTO.java
+++ src/main/java/jp/co/axisb/dto/AdministrarorsDTO.java
@@ -1,58 +1,29 @@
 package jp.co.axisb.dto;
 
 public class AdministrarorsDTO {
-<<<<<<< HEAD
 	
 	public String getAdminId() {
-=======
-
-	private int adminId;
-	private int password;
-	private String name;
-	
-	public int getAdminId() {
->>>>>>> branch 'master' of https://github.com/masakazu-ishida/TeamB.git
 		return adminId;
 	}
-<<<<<<< HEAD
 	public void setAdminId(String adminId) {
-=======
-
-	public void setAdmin_id(int adminId) {
->>>>>>> branch 'master' of https://github.com/masakazu-ishida/TeamB.git
 		this.adminId = adminId;
 	}
-<<<<<<< HEAD
 	public String getPassword() {
-=======
-
-	public int getPassword() {
->>>>>>> branch 'master' of https://github.com/masakazu-ishida/TeamB.git
 		return password;
 	}
-<<<<<<< HEAD
 	public void setPassword(String password) {
-=======
-
-	public void setPassword(int password) {
->>>>>>> branch 'master' of https://github.com/masakazu-ishida/TeamB.git
 		this.password = password;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-<<<<<<< HEAD
 	private String adminId;
 	private String password;
 	private String name;
 	
 	
-=======
->>>>>>> branch 'master' of https://github.com/masakazu-ishida/TeamB.git
 
 }
