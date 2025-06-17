d873fcb6c56e050ed8d2610c8266d24baecacd9c
morio-j <morio-j@AXPC-1554N.TTRDOM.local>
2025-06-17 13:04:57 +0900

    ｌ

--- src/main/java/jp/co/axisb/controller/SearchController.java
+++ src/main/java/jp/co/axisb/controller/SearchController.java
@@ -1,14 +1,17 @@
 package jp.co.axisb.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Service.SearchService;
+import jp.co.axisb.dto.ItemsDTO;
+import jp.co.axisb.service.SearchService;
 
 /**
  * Servlet implementation class SearchController
@@ -43,19 +46,26 @@ public class SearchController extends HttpServlet {
 		// TODO Auto-generated method stub
 		//doGet(request, response);]
 
+		String path = "/main/WEB-INF/search.jsp";
+
 		//リクエストパラメータを取り出す
-		request.getParameter();
-		//サーチサービスをインスタンス化
-		SearchService search = new SearchService();
+		String keyword = request.getParameter("keyword");
+		String categoriesId = request.getParameter("categoriesId");
 
 		//サーチサービスのサーチメソッドを呼び出す
-
 		//引数には検索キーワードとカテゴリIDを渡す
+		SearchService.search(keyword, categoriesId);
 
 		//サービスからの戻り値をセットアトリビュートする
+		List<ItemsDTO> s = SearchService.search(keyword, categoriesId);
+		なんでDTOのリスト？
+		
+
+		request.setAttribute("value", s);
 
 		//フォワード
-
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
 	}
 
 }
---
+++ src/main/java/jp/co/axisb/service/SearchService.java
@@ -0,0 +1,24 @@
+package jp.co.axisb.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import jp.co.axisb.dao.ItemsDAO;
+import jp.co.axisb.dto.ItemsDTO;
+
+public class SearchService {
+
+	public static List<ItemsDTO> search(String keyword, String categoriesId) {
+
+		//daoのfindbyItemnameの呼び出し
+		ItemsDAO dao = new ItemsDAO();
+		
+		//引数を渡す
+		引数の渡し方がわからん！
+
+		//if文で全部の処理
+
+	
+	}
+
+}
