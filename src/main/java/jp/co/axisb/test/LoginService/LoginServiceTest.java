763db609c41e9d320334f092c0a894d1ac6d0d48
tanaka-yu <tanaka-yu@AXPC-1476N.TTRDOM.local>
2025-06-18 13:08:17 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

---
+++ src/main/java/jp/co/axisb/test/LoginService/LoginServiceTest.java
@@ -0,0 +1,42 @@
+package jp.co.axisb.test.LoginService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jp.co.axisb.dao.BaseDAO;
+import jp.co.axisb.dto.UsersDTO;
+import jp.co.axisb.service.LoginService;
+import jp.co.axisb.util.ConnectionUtil;
+
+public class LoginServiceTest {
+
+	@BeforeEach
+	void init() {
+		try (Connection conn = ConnectionUtil.getConnectionForJUnit()) {
+
+			BaseDAO dao = new BaseDAO(conn);
+			try {
+				dao.insertBatch("sqlFiles/init.sql");
+
+			} catch (Exception e) {
+				throw e;
+			}
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testFindById() {
+		UsersDTO search = LoginService.login("user");
+		assertNotNull(search);
+
+		search = LoginService.login("aaaaaaaaa");
+		assertNull(search);
+	}
+
+}
