e47c7008ddff2a1bcb5c67f51ea8f01bf1d26847
tanaka-yu <tanaka-yu@AXPC-1476N.TTRDOM.local>
2025-06-18 10:28:25 +0900

    Date

--- src/main/java/jp/co/axisb/dao/ItemsInCartDAO.java
+++ src/main/java/jp/co/axisb/dao/ItemsInCartDAO.java
@@ -54,7 +54,7 @@ public class ItemsInCartDAO extends BaseDAO {
 				dto.setUserId(rs.getString("itemsInCartUserId"));
 				dto.setItemId(rs.getInt("itemsInCartItemId"));
 				dto.setAmount(rs.getInt("amount"));
-				dto.setBookedDate(rs.getDate("booked_date"));
+				dto.setBookedDate((java.util.Date) rs.getDate("booked_date"));
 				dto.setUsers(users);
 				dto.setItems(items);
 
@@ -103,7 +103,7 @@ public class ItemsInCartDAO extends BaseDAO {
 				dto.setUserId(rs.getString("itemsInCartUserId"));
 				dto.setItemId(rs.getInt("itemsInCartItemId"));
 				dto.setAmount(rs.getInt("amount"));
-				dto.setBookedDate(rs.getDate("booked_date"));
+				dto.setBookedDate((java.util.Date) rs.getDate("booked_date"));
 				dto.setUsers(users);
 				dto.setItems(items);
 
@@ -150,7 +150,7 @@ public class ItemsInCartDAO extends BaseDAO {
 				dto.setUserId(rs.getString("itemsInCartUserId"));
 				dto.setItemId(rs.getInt("itemsInCartItemId"));
 				dto.setAmount(rs.getInt("amount"));
-				dto.setBookedDate(rs.getDate("booked_date"));
+				dto.setBookedDate((java.util.Date) rs.getDate("booked_date"));
 				dto.setUsers(users);
 				dto.setItems(items);
 
