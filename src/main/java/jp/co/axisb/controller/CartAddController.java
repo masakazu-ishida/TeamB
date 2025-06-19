bf852bc9c09d0fdb91f7eca5ed2f6c28b3461e85
tanaka-yu <tanaka-yu@AXPC-1476N.TTRDOM.local>
2025-06-19 14:31:53 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

--- src/main/java/jp/co/axisb/controller/CartAddController.java
+++ src/main/java/jp/co/axisb/controller/CartAddController.java
@@ -1,6 +1,7 @@
 package jp.co.axisb.controller;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import jp.co.axisb.service.CartService;
 
 /**
  * Servlet implementation class CartAddController
@@ -43,14 +46,20 @@ public class CartAddController extends HttpServlet {
 		//doGet(request, response);
 		HttpSession session = request.getSession(true);
 
-		String userId = (String) session.getAttribute("会員ID");
+		String userId = (String) request.getAttribute("会員ID");
+		int itemId = (int)request.getAttribute("商品ID");
+		Date bookedDate = (Date)request.getAttribute("購入日");
+		int amount = (int)request.getAttribute("数量");
+		
 
 		if (userId == null) {
 			response.sendRedirect("/axis_b/LoginController");
 
 		} else {
+			
+			CartService.Cartadd(userId, 0, null, 0)
 
-			RequestDispatcher rd = request.getRequestDispatcher("カート一覧表示のサーブレットのパス");
+			RequestDispatcher rd = request.getRequestDispatcher("/axis_b/CartController");
 			rd.forward(request, response);
 
 		}
