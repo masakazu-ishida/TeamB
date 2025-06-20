87469366b97834a61c84e52ce9fb4828b50c9363
tanaka-yu <tanaka-yu@AXPC-1476N.TTRDOM.local>
2025-06-20 10:45:04 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

--- src/main/java/jp/co/axisb/service/AdminMainService.java
+++ src/main/java/jp/co/axisb/service/AdminMainService.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 
 import jp.co.axisb.dao.AdministratorsDAO;
 import jp.co.axisb.dto.AdministratorsDTO;
+import jp.co.axisb.util.CommonConstants;
 import jp.co.axisb.util.ConnectionUtil;
 
 public class AdminMainService {
 
 	public static AdministratorsDTO login(String userId, String password) {
-		try (Connection conn = ConnectionUtil.getConnectionForJUnit()) {
+		try (Connection conn = ConnectionUtil.getConnection(CommonConstants.LOOKUP_NAME)) {
 			AdministratorsDAO dao = new AdministratorsDAO(conn);
 
 			return dao.findById(userId, password);
