e90a7c7d2fb3a675612b0b6117b32955fae068ff
murao-s-hue <murao-s@ita-school.jp>
2026-06-16 15:41:52 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamB.git

--- src/main/java/jp/co/ramen/dao/ItemsInCartDAO.java
+++ src/main/java/jp/co/ramen/dao/ItemsInCartDAO.java
@@ -84,28 +84,43 @@ public class ItemsInCartDAO {
 	}
 
 	//	以下、開発途中(？2つ）（村尾担当）
-	public ItemsInCartDTO findByDoubleID(String SESSION) throws SQLException {
-		String sql = "SELECT items.name, items.manufacturer, items.price, items_in_cart.amount \n"
+	public ArrayList<ItemsInCartDTO> findByDoubleID(String SESSION) throws SQLException {
+		String sql = "SELECT items.item_id,items.name,items.manufacturer, items.category_id, items.color,"
+				+ " items.price, items.stock, items.recommended,"
+				+ "items_in_cart.user_id, items_in_cart.item_id,"
+				+ " items_in_cart.amount, items_in_cart.booked_date \n"
 				+ "FROM items_in_cart \n"
 				+ "INNER JOIN items ON items_in_cart.item_id = items.item_id\n"
 				+ "WHERE items_in_cart.user_id = ?";
-		ItemsInCartDTO IicDto = null;
-		ItemsDTO Idto = null;
+		ArrayList<ItemsInCartDTO> iicList = new ArrayList<>();
 		try (PreparedStatement ps = con.prepareStatement(sql)) {
 			ps.setString(1, SESSION);
+			System.out.println(ps);
 			try (ResultSet rs = ps.executeQuery()) {
 				while (rs.next()) {
 					//mapRowはResultSetからDTOへの変換メソッド。複数箇所で利用するので共通化
-					Idto.setName(rs.getString("name"));
-					Idto.setManufacturer(rs.getString("manufacturer"));
-					Idto.setPrice(rs.getInt("amount"));
-					IicDto.setItemsDto(Idto);
-					IicDto.setAmount(rs.getInt("amount"));
-					return IicDto;
+					ItemsInCartDTO iicDto = new ItemsInCartDTO();
+					ItemsDTO idto = new ItemsDTO();
+
+					idto.setItem_id(rs.getInt("item_id"));
+					idto.setName(rs.getString("name"));
+					idto.setManufacturer(rs.getString("manufacturer"));
+					idto.setCategory_id(rs.getInt("category_id"));
+					idto.setColor(rs.getString("color"));
+					idto.setPrice(rs.getInt("price"));
+					idto.setStock(rs.getInt("stock"));
+					idto.setRecommended(rs.getBoolean("recommended"));
+					iicDto.setItemsDto(idto);
+
+					iicDto.setUser_id(rs.getString("user_id"));
+					iicDto.setItem_id(rs.getInt("item_id"));
+					iicDto.setAmount(rs.getInt("amount"));
+					iicDto.setBooked_date(rs.getDate("booked_date"));
+					iicList.add(iicDto);
 				}
 			}
-		}
-		return null; // 見つからない場合はnullを返す
-	}
 
+			return iicList;// 見つからない場合はnullを返す
+		}
+	}
 }
