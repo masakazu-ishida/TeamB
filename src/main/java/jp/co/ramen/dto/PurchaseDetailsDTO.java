c16d737f42dd4952c8b39b4025bc55e0fd4ed283
aobara-bara <kikuchi-m@t-axis.co.jp>
2026-06-16 13:31:43 +0900

    コミット確認

--- src/main/java/jp/co/ramen/dto/PurchaseDetailsDTO.java
+++ src/main/java/jp/co/ramen/dto/PurchaseDetailsDTO.java
@@ -2,12 +2,12 @@ package jp.co.ramen.dto;
 
 public class PurchaseDetailsDTO {
 
-	private String purchase_detail_id;
-	private String purchase_id;
-	private String item_id;
+	private int purchase_detail_id;
+	private int purchase_id;
+	private int item_id;
 	private int amount;
 
-	public PurchaseDetailsDTO(String purchase_detail_id, String purchase_id, String item_id, int amount) {
+	public PurchaseDetailsDTO(int purchase_detail_id, int purchase_id, int item_id, int amount) {
 		this.purchase_detail_id = purchase_detail_id;
 		this.purchase_id = purchase_id;
 		this.item_id = item_id;
@@ -18,27 +18,27 @@ public class PurchaseDetailsDTO {
 
 	}
 
-	public String getPurchase_detail_id() {
+	public int getPurchase_detail_id() {
 		return purchase_detail_id;
 	}
 
-	public void setPurchase_detail_id(String purchase_detail_id) {
+	public void setPurchase_detail_id(int purchase_detail_id) {
 		this.purchase_detail_id = purchase_detail_id;
 	}
 
-	public String getPurchase_id() {
+	public int getPurchase_id() {
 		return purchase_id;
 	}
 
-	public void setPurchase_id(String purchase_id) {
+	public void setPurchase_id(int purchase_id) {
 		this.purchase_id = purchase_id;
 	}
 
-	public String getItem_id() {
+	public int getItem_id() {
 		return item_id;
 	}
 
-	public void setItem_id(String item_id) {
+	public void setItem_id(int item_id) {
 		this.item_id = item_id;
 	}
 
