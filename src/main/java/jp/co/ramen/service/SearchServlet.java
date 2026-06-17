5f19cb579729d425b95f502410c17b05b92a633f
Honoka-m <mogami-h@ita-school.jp>
2026-06-17 11:01:50 +0900

    SearchServlet

---
+++ src/main/java/jp/co/ramen/service/SearchServlet.java
@@ -0,0 +1,61 @@
+package jp.co.ramen.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jp.co.ramen.dto.ItemsDTO;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		String name = request.getParameter("name");
+
+		SearchServletService itemSearch = new SearchServletService();
+
+		List<ItemsDTO> itemsList = itemSearch.execute(categoryId, name);
+
+		request.setAttribute("itemsList", itemsList);
+
+		String path = "/itemsResult.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
