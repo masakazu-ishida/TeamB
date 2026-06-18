57b6b49ccfdb7d3ebf383ebba2689743a2cc1c48
kacyanAxis <takahashi-ka@ita-school.jp>
2026-06-18 10:56:07 +0900

    不使用メソッドのコメントアウト_taka

--- src/main/java/jp/co/ramen/dao/UserDAO.java
+++ src/main/java/jp/co/ramen/dao/UserDAO.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import jp.co.ramen.dto.UserDTO;
 
@@ -16,16 +14,16 @@ public class UserDAO {
 		this.con = con;
 	}
 
-	// データの挿入 (Create)
-	public int insert(UserDTO user) throws SQLException {
-		String sql = "INSERT INTO public.users (user_id, password, name, address) VALUES (?, ?, ?, ?)";
-		try (PreparedStatement ps = con.prepareStatement(sql)) {
-			//ps.setString(1, user.getUserId());
-			ps.setString(2, user.getPassword());
-			ps.setString(3, user.getName());
-			ps.setString(4, user.getAddress());
-			return ps.executeUpdate();
-		}
+	//	ResultSetからエンティティへのマッピングを行う共通メソッド
+	//	主キー検索と全件検索で重複を防ぐため
+
+	private UserDTO mapRow(ResultSet rs) throws SQLException {
+		UserDTO user = new UserDTO();
+		user.setUserId(rs.getString("user_id"));
+		user.setPassword(rs.getString("password"));
+		user.setName(rs.getString("name"));
+		user.setAddress(rs.getString("address"));
+		return user;
 	}
 
 	// 主キーによる検索 
@@ -47,49 +45,51 @@ public class UserDAO {
 	}
 
 	// 全件検索 
-	public List<UserDTO> findAll() throws SQLException {
-		String sql = "SELECT user_id, password, name, address FROM public.users";
-		List<UserDTO> list = new ArrayList<>();
-		try (PreparedStatement ps = con.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery()) {
-			while (rs.next()) {
-				//mapRowはResultSetからDTOへの変換メソッド。複数箇所で利用するので共通化
-				UserDTO user = mapRow(rs);
-				list.add(user);
-			}
-		}
-		return list;
-	}
+	//	public List<UserDTO> findAll() throws SQLException {
+	//		String sql = "SELECT user_id, password, name, address FROM public.users";
+	//		List<UserDTO> list = new ArrayList<>();
+	//		try (PreparedStatement ps = con.prepareStatement(sql);
+	//				ResultSet rs = ps.executeQuery()) {
+	//			while (rs.next()) {
+	//				//mapRowはResultSetからDTOへの変換メソッド。複数箇所で利用するので共通化
+	//				UserDTO user = mapRow(rs);
+	//				list.add(user);
+	//			}
+	//		}
+	//		return list;
+	//	}
 
 	// データの更新 
-	public int update(UserDTO user) throws SQLException {
-		String sql = "UPDATE public.users SET password = ?, name = ?, address = ? WHERE user_id = ?";
-		try (PreparedStatement ps = con.prepareStatement(sql)) {
-			ps.setString(1, user.getPassword());
-			ps.setString(2, user.getName());
-			ps.setString(3, user.getAddress());
-			//	ps.setString(4, user.getUserId());
-			return ps.executeUpdate();
-		}
-	}
+	//	public int update(UserDTO user) throws SQLException {
+	//		String sql = "UPDATE public.users SET password = ?, name = ?, address = ? WHERE user_id = ?";
+	//		try (PreparedStatement ps = con.prepareStatement(sql)) {
+	//			ps.setString(1, user.getPassword());
+	//			ps.setString(2, user.getName());
+	//			ps.setString(3, user.getAddress());
+	//			ps.setString(4, user.getUserId());
+	//			return ps.executeUpdate();
+	//		}
+	//	}
 
 	// データの削除
-	public int delete(String userId) throws SQLException {
-		String sql = "DELETE FROM public.users WHERE user_id = ?";
-		try (PreparedStatement ps = con.prepareStatement(sql)) {
-			ps.setString(1, userId);
-			return ps.executeUpdate();
-		}
-	}
+	//	public int delete(String userId) throws SQLException {
+	//		String sql = "DELETE FROM public.users WHERE user_id = ?";
+	//		try (PreparedStatement ps = con.prepareStatement(sql)) {
+	//			ps.setString(1, userId);
+	//			return ps.executeUpdate();
+	//		}
+	//	}
 
-	// ResultSetからエンティティへのマッピングを行う共通メソッド
-	//主キー検索と全件検索で重複を防ぐため
-	private UserDTO mapRow(ResultSet rs) throws SQLException {
-		UserDTO user = new UserDTO();
-		//	user.setUserId(rs.getString("user_id"));
-		user.setPassword(rs.getString("password"));
-		user.setName(rs.getString("name"));
-		user.setAddress(rs.getString("address"));
-		return user;
-	}
+	// データの挿入 (Create)
+	//	public int insert(UserDTO user) throws SQLException {
+	//		String sql = "INSERT INTO public.users (user_id, password, name, address) VALUES (?, ?, ?, ?)";
+	//		try (PreparedStatement ps = con.prepareStatement(sql)) {
+	//			ps.setString(1, user.getUserId());
+	//			ps.setString(2, user.getPassword());
+	//			ps.setString(3, user.getName());
+	//			ps.setString(4, user.getAddress());
+	//			return ps.executeUpdate();
+	//		}
+	//	}
+
 }
