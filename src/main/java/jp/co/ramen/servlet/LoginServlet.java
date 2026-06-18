2701081b8206c7f7eaa219e539a1193fdc7cbb1b
kacyanAxis <takahashi-ka@ita-school.jp>
2026-06-18 13:38:04 +0900

    L68_ログイン失敗時の追加商品の情報受け渡し処理を実装_taka

--- src/main/java/jp/co/ramen/servlet/LoginServlet.java
+++ src/main/java/jp/co/ramen/servlet/LoginServlet.java
@@ -23,7 +23,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	public LoginServlet() {
 		super();
-		// TODO Auto-generated constructor stub
+		// TODO Auto- generated constructor stub
 	}
 
 	/**
