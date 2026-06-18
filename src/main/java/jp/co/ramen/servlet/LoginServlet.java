64da1977988d982e7963fda5308fc54f6afd66d7
kacyanAxis <takahashi-ka@ita-school.jp>
2026-06-18 13:40:31 +0900

    L68_ログイン失敗時の追加商品のデータ譲渡処理を実装_taka

--- src/main/java/jp/co/ramen/servlet/LoginServlet.java
+++ src/main/java/jp/co/ramen/servlet/LoginServlet.java
@@ -74,7 +74,7 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see Htt pServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
