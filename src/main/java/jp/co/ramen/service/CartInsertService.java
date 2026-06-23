16d0a5d78761867d3ffd24ff00427bf3ec203fe1
murao-s-hue <murao-s@ita-school.jp>
2026-06-23 09:21:02 +0900

    カート追加処理修正　村

--- src/main/java/jp/co/ramen/service/CartInsertService.java
+++ src/main/java/jp/co/ramen/service/CartInsertService.java
@@ -18,45 +18,50 @@ public class CartInsertService {
 		String jndiName = "java:comp/env/jdbc/ecsite";
 		try (Connection conn = ConnectionUtil.getConnection(jndiName)) {
 
-			ItemsInCartDAO cartDAO = new ItemsInCartDAO(conn);
-
-			// カート内に同じ商品があるかチェック
-			ItemsInCartDTO cartDto = cartDAO.findByUserIdAndItemId(userId, itemId);
-			//日付	
-			LocalDate bookedDate = LocalDate.now();
-
 			conn.setAutoCommit(false);
+
 			try {
-				if (cartDto != null) {
-					// 既にカートにある場合は、元々の数量にorderを加算
-					int newAmount = cartDto.getAmount() + order;
+				ItemsInCartDAO cartDAO = new ItemsInCartDAO(conn);
 
-					// カート内の数量と更新日を更新
-					cartDAO.update(userId, itemId, newAmount, bookedDate);
+				// カート内に同じ商品があるかチェック
+				ItemsInCartDTO cartDto = cartDAO.findByUserIdAndItemId(userId, itemId);
+				//日付	
+				LocalDate bookedDate = LocalDate.now();
+				int result;
 
-				} else {
-					// カートにない場合は、orderを数量として新規登録
-					ItemsInCartDTO insertDto = new ItemsInCartDTO();
-					insertDto.setUser_id(userId);
-					insertDto.setItem_id(itemId);
-					insertDto.setAmount(order);
+				try {
+					if (cartDto != null) {
+						// 既にカートにある場合は、元々の数量にorderを加算
+						int newAmount = cartDto.getAmount() + order;
 
-					insertDto.setBooked_date(bookedDate);
+						// カート内の数量と更新日を更新
+						cartDAO.update(userId, itemId, newAmount, bookedDate);
 
-					cartDAO.insert(insertDto);
+					} else {
+						// カートにない場合は、orderを数量として新規登録
+						ItemsInCartDTO insertDto = new ItemsInCartDTO();
+						insertDto.setUser_id(userId);
+						insertDto.setItem_id(itemId);
+						insertDto.setAmount(order);
+
+						insertDto.setBooked_date(bookedDate);
+
+						result = cartDAO.insert(insertDto);
+					}
+
+					conn.commit();
+				} catch (Exception e) {
+
+					e.printStackTrace();
+					conn.rollback();
 				}
 
-				conn.commit();
 			} catch (Exception e) {
-				e.printStackTrace();
-				conn.rollback();
+
+				throw new ServletException(e.getCause());
+
 			}
 
-		} catch (Exception e) {
-
-			throw new ServletException(e.getCause());
-
 		}
-
 	}
 }
